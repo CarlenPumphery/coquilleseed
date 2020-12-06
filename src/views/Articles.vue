c835bf58d660eafe7d43840260196ded11ea9293
liquidseymour <connect@carlen.tech>
2020-12-05 16:22:49 -0800

    skeleton

---
+++ src/views/Articles.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "Articles.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Links.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "Links.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
