f49d838152ba6ec73f5a9354c5d81e35f90a00a4
carlen <carlen@underlineepic.com>
2020-12-26 08:57:01 -0800

    Fixed gallery

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -129,8 +129,8 @@
                   cols="4"
               >
                 <v-img
-                    :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
-                    :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
+                    :src="`https://picsum.photos/500/300?image=${n * 10 + 3}`"
+                    :lazy-src="`https://picsum.photos/10/6?image=${n * 10- + 3}`"
                     aspect-ratio="1"
                     class="grey lighten-2"
                 >
